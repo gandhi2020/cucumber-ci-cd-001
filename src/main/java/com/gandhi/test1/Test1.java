a3623ae14e736150069822bdfdcec1f6e40795cf
gandhi2020 <10249739+gandhi2020@users.noreply.github.com>
2022-09-30 15:40:30 +0530

    Adding first set of changes

---
+++ src/main/java/com/gandhi/test1/Test1.java
@@ -0,0 +1,10 @@
+package com.gandhi.test1;
+
+public class Test1 {
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("adding second line to print statement");
+        
+    }
+}
