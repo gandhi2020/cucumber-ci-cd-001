e6d03f3bf7e7cafbbb591374d7b3bb313b4a7690
gandhi2020 <10249739+gandhi2020@users.noreply.github.com>
2023-04-15 15:26:57 +0530

    added new class Test2.java

---
+++ src/main/java/com/gandhi/test1/Test2.java
@@ -0,0 +1,10 @@
+package com.gandhi.test1;
+
+public class Test2 {
+//    System.out.println("hello world");
+    public static void main(String args[]){
+        System.out.println("Testing my knowledge on git version control");
+        System.out.println("this file is not added to git");
+    }
+
+}
